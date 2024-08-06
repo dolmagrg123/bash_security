69ebefb7f6d8f6f19930604030cec7cb5a5544d1
Dolma <dolmatgurung1@gmail.com>
2024-08-06 02:23:49 +0000

    Fixed the destination path of suspicious log file

--- analyze_suspicious.sh
+++ analyze_suspicious.sh
@@ -2,13 +2,11 @@
 
 #declare the source file where we will check
 source_file="/var/log/auth_log.log"
-#declare the destination file where we will copy the lines  with suspicious words
-destination_file="suspicious_activity.log"
 
-# Check if the file exists and create file if it does not exists
-if [[ ! -f "$destination_file" ]]; then
-  touch "$destination_file"
-fi
+#declare the destination path of the file where we will copy the lines  with suspicious words
+destination_path="/home/ubuntu/bash_security"
+#create path if it does not exists
+mkdir -p "$destination_path" 
 
 #words that are suspicious in the log file
 keywords=("failed" "invalid" "error" "pam" "unauthorized" "failure")
@@ -17,7 +15,8 @@ keywords=("failed" "invalid" "error" "pam" "unauthorized" "failure")
 while read -r line; do
 	for word in "${keywords[@]}"; do #check each word in the list
 		if echo "$line" | grep -qi "$word"; then # check for each word in line ignoring case
-			echo "$line" >> "$destination_file" #copy the line with suspicious word to our file
+			cd "$destination_path"
+			echo "$line" >> "suspicious_activity.log" #copy the line with suspicious word to our file
 			continue 2 #move to next line if any word is found to avoid copying same line twice
 		fi
 	done
